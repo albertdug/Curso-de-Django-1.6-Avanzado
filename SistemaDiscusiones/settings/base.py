f41d9c308cea16a50d303b52bab9253851c3a7cb
Albert <albertrdg@gmail.com>
2016-04-18 00:03:46 -0300

    Config project

---
+++ SistemaDiscusiones/settings/base.py
@@ -0,0 +1,49 @@
+import os
+from unipath import Path
+BASE_DIR = Path(__file__).ancestor(3)
+
+SECRET_KEY = '0!$vhwt!%7kl-m^+*ny=-j--nou0klub%oc^lk*6f2_stdobru'
+
+DJANGO_APPS = (
+		'django.contrib.admin',
+	    'django.contrib.auth',
+	    'django.contrib.contenttypes',
+	    'django.contrib.sessions',
+	    'django.contrib.messages',
+	    'django.contrib.staticfiles',
+	)
+
+THIRD_PARTY_APPS = (
+
+	)
+
+LOCAL_APPS = (
+
+	)
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'SistemaDiscusiones.urls'
+
+WSGI_APPLICATION = 'SistemaDiscusiones.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+TEMPLATES_DIRS = [BASE_DIR.child('templates')]
---
+++ SistemaDiscusiones/settings/local.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
